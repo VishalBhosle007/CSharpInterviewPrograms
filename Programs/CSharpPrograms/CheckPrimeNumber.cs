e194cb6a93ee4bf19eb54b1d099662c1299c1c13
Vishal Bhosle <vishalbhosle1432@gmail.com>
2025-12-10 21:50:38 +0530

    Check given number is Prime or not

---
+++ Programs/CSharpPrograms/CheckPrimeNumber.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programs.CSharpPrograms
+{
+    public class CheckPrimeNumber : IProgram
+    {
+        public string Name => "Check given number is Prime or not";
+
+        public void Run()
+        {
+            Console.WriteLine("Enter a number to check if it is prime:");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n <= 1)
+            {
+                Console.WriteLine($"{n} is not a prime number.");
+                return;
+            }
+
+            bool isPrime = true;
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime)
+            {
+                Console.WriteLine($"{n} is a prime number.");
+            }
+            else
+            {
+                Console.WriteLine($"{n} is not a prime number.");
+            }
+        }
+    }
+}
