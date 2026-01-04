8829a9c060c204a316c32672ae79066271cde15d
Vishal Bhosle <vishalbhosle1432@gmail.com>
2026-01-04 23:05:47 +0530

    Reverse a number

---
+++ Programs/CSharpPrograms/String Programs/ReverseNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programs.CSharpPrograms.String_Programs
+{
+    public class ReverseNumber : IProgram
+    {
+        public string Name => "Reverse a number";
+
+        public void Run()
+        {
+            Console.WriteLine("Enter number to reverse");
+            int input = int.Parse(Console.ReadLine());
+
+            int reversedNumber = 0;
+            while (input > 0)
+            {
+                int digit = input % 10;
+                reversedNumber = (reversedNumber * 10) + digit;
+                input /= 10;
+            }
+
+            Console.WriteLine($"Reversed Number is {reversedNumber}");
+        }
+    }
+}
