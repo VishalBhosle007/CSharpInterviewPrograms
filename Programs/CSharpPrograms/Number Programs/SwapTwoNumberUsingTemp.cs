847f5d82b0179cdc3581199abb6e6d6bd7ee2c43
Vishal Bhosle <vishalbhosle1432@gmail.com>
2026-01-10 22:00:29 +0530

    Swap two number using third variable

---
+++ Programs/CSharpPrograms/Number Programs/SwapTwoNumberUsingTemp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programs.CSharpPrograms.Number_Programs
+{
+    public class SwapTwoNumberUsingTemp : IProgram
+    {
+        public string Name => "Swap two number using third/temp variable";
+
+        public void Run()
+        {
+            Console.Write("Enter value for A :");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter value for B :");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Before Swapping: a = {a}, b = {b}");
+
+            // Swapping using a temporary variable
+            int temp = a;
+            a = b;
+            b = temp;
+
+            Console.WriteLine($"After Swapping: a = {a}, b = {b}");
+        }
+    }
+}
