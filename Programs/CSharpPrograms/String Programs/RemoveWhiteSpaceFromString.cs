5dc1366ad1335257a9e42d6c26309e1b40b44147
Vishal Bhosle <vishalbhosle1432@gmail.com>
2026-01-14 11:52:30 +0530

    Remove all white spaces from a string.

---
+++ Programs/CSharpPrograms/String Programs/RemoveWhiteSpaceFromString.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programs.CSharpPrograms.String_Programs
+{
+    public class RemoveWhiteSpaceFromString : IProgram
+    {
+        public string Name => "Remove all white spaces from a string.";
+
+        public void Run()
+        {
+            Console.WriteLine("Enter a string to remove all white spaces:");
+            string input = Console.ReadLine() ?? "";
+
+            StringBuilder resultBuilder = new StringBuilder();
+            foreach (char c in input)
+            {
+                if (!char.IsWhiteSpace(c))
+                {
+                    resultBuilder.Append(c);
+                }
+            }
+            Console.WriteLine(resultBuilder.ToString());
+        }
+    }
+}
