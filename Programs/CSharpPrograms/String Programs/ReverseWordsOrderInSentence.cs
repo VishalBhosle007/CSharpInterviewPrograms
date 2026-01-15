0d64bf069f89994f7dc01be19d334873de579b45
Vishal Bhosle <vishalbhosle1432@gmail.com>
2026-01-15 15:17:01 +0530

    Reverse words order in sentence

---
+++ Programs/CSharpPrograms/String Programs/ReverseWordsOrderInSentence.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programs.CSharpPrograms.String_Programs
+{
+    public class ReverseWordsOrderInSentence : IProgram
+    {
+        public string Name => "Reverse words order in sentence";
+
+        public void Run()
+        {
+            Console.WriteLine("Enter string to reverse words order");
+            string str = Console.ReadLine();
+
+            StringBuilder result = new StringBuilder();
+            StringBuilder word = new StringBuilder();
+
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                if (str[i] != ' ')
+                {
+                    // Build word in reverse order
+                    word.Insert(0, str[i]);
+                }
+                else
+                {
+                    // Append complete word
+                    result.Append(word);
+                    result.Append(' ');
+                    word.Clear();
+                }
+            }
+
+            // Append first word
+            result.Append(word);
+
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
