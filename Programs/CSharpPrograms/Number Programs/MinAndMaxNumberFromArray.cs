a5cca261aab817606d8475e7e56b4edc25855f64
Vishal Bhosle <vishalbhosle1432@gmail.com>
2026-01-17 17:17:29 +0530

    Find the maximum and minimum number from array

---
+++ Programs/CSharpPrograms/Number Programs/MinAndMaxNumberFromArray.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programs.CSharpPrograms.Number_Programs
+{
+    public class MinAndMaxNumberFromArray : IProgram
+    {
+        public string Name => "Find minimum and maximum number form array";
+
+        int[] arr = { 22, 34, 59, 38, 39, 94, 27, 4, 28, 19 };
+
+        public void Run()
+        {
+            int min = arr[0];
+            int max = arr[0];
+
+            foreach (int num in arr)
+            {
+                //Minimum
+                if (min > num)
+                {
+                    min = num;
+                }
+
+                //Maximum
+                if (max < num)
+                {
+                    max = num;
+                }
+            }
+
+            Console.WriteLine($"Minimum number is : {min}\nMaximum number is : {max}");
+        }
+    }
+}
