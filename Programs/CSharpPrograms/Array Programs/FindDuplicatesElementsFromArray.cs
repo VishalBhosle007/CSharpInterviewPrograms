3b606391c35d899d170cb99f1e55a6d5d8a3ad8a
Vishal Bhosle <vishalbhosle1432@gmail.com>
2026-02-24 18:13:17 +0530

    Find all duplicate elements from array

---
+++ Programs/CSharpPrograms/Array Programs/FindDuplicatesElementsFromArray.cs
@@ -0,0 +1,35 @@
+﻿using Programs.CSharpPrograms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpInterviewPrograms.CSharpPrograms.Array_Programs
+{
+    public class FindDuplicatesElementsFromArray : IProgram
+    {
+        public string Name => "Find all duplicate elements from array";
+
+        public void Run()
+        {
+            int[] arr = { 22, 23, 59, 39, 33, 22, 30, 49, 23, 33 };
+            HashSet<int> visited = new HashSet<int>();
+            List<int> duplicates = new List<int>();
+
+            foreach (int i in arr)
+            {
+                if (!visited.Add(i))
+                {
+                    duplicates.Add(i);
+                }
+            }
+
+            if (duplicates.Any())
+            {
+                Console.WriteLine(string.Join(", ", duplicates));
+            }
+
+        }
+    }
+}
